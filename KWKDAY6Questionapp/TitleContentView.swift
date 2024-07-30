b6b155449c52f3ead5ab905f72497ecc04c9ae2b
Rachel Auguste <rachelauguste36@gmail.com>
2024-07-30 11:17:54 -0400

    fixed error

---
+++ KWKDAY6Questionapp/TitleContentView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleContentView.swift
+//  KWKDAY6Questionapp
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct TitleContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleContentView()
+}
